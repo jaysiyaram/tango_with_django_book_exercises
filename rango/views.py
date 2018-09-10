9a1a74d72f1ce9cf2234502da1248add27861640
ramanuj <ramanuj.sharma512@gmail.com>
2018-09-10 22:37:31 +0530

    Adding chapter 7 code

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category
+from rango.models import Category, Page
 
 def index(request):
     # context_dict = {'boldmessage': "This is a bold message to be published"}
@@ -13,3 +13,21 @@ def about(request):
     context_dict = {'notboldmessage': "This is being done to learn something small yet significant"}
     return render(request, 'rango/about.html', context_dict)
     #return HttpResponse("Rango says here is the about page")
+
+def category(request, category_name_slug):
+    context_dict = {}
+    
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+
+        pages = Pages.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
+
