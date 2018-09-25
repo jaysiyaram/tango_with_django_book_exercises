e64105bf9b98443d60b94984e6ee7f3ab0ef03dd
ramanuj <ramanuj.sharma512@gmail.com>
2018-09-25 23:11:35 +0530

    Adding some code from Chapter 9

--- rango/admin.py
+++ rango/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 
 #class PageAdmin(admin.ModelAdmin):
@@ -13,3 +13,5 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(Category)
 # admin.site.register(Page, PageAdmin)
 admin.site.register(Page)
+
+admin.site.register(UserProfile)
--- rango/urls.py
+++ rango/urls.py
@@ -8,7 +8,8 @@ urlpatterns = patterns('',
         url(r'^add_category/$', views.add_category, name='add_category'),
         url(r'^add_page/$', views.add_page, name='add_page_render'),
         url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
-        url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category') # New!
+        url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
+        url(r'^register/$', views.register, name='register')
         )
 
 if settings.DEBUG:
